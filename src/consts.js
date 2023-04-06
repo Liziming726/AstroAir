d333d48cfc93da5cccb7a6f1c27e2c696c4ec51d
Liziming726 <102896505+Liziming726@users.noreply.github.com>
2023-04-06 11:08:49 +0800

    initial commit

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Air Blog`;
+export const SITE_TITLE = `li's Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_EMAIL = '873884635@qq.com'
+export const SITE_NAME = 'astro.liziming.com';
+export const SITE_URL = "https://astro.liziming.com";
